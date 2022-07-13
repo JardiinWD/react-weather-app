5216fbf7ecdbde433d6cb06296b5a458725c06ef
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-13 20:26:38 +0200

    Aggiunto la funzione per data e localizzazione

--- src/App.js
+++ src/App.js
@@ -12,18 +12,51 @@ const api = {
 console.log(api);
 
 function App() {
+  /* Arrow-function per la mia date */
+  const dateBuilder = (d) => {
+    /* Mesi */
+    const mesi = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio",
+      "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+    /* Giorni */
+    const days = ["Lunedi", "Martedi", "Mercoledi", "Giovedi", "Venerdi", "Sabato", "Domenica"]
+    /* Ora faccio il modo che un giorno venga pescato correttamente dai vari metodi */
+    let giorno = days[d.getDay()];
+    let data = d.getDate();
+    let mese = mesi[d.getMonth()];
+    let anno = d.getFullYear();
+    /* Ora tramite return pesco tutti i dati */
+    return `${giorno} ${data} ${mese} ${anno}`
+  }
+
   return (
-    <div className="app warm">
+    <div className="app">
       <main>
         {/* Searchbox necessaria */}
         <div className="search-box">
+          {/* Input della searchbox */}
           <input
             type="text"
             className="search-bar"
             placeholder="Cerca.."
           />
         </div>
-
+        {/* Box della location */}
+        <div className="location-box">
+          {/* Locazione */}
+          <div className="location">San Paolo d'Argon, BG</div>
+          {/* Data 
+          dateBuilder => la mia costante, l'array di variabili di array sopra return
+          new Date() => istanza
+          */}
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        {/* Box per il tempo */}
+        <div className="weather-box">
+          {/* Temperatura va qua */}
+          <div className="temperature"></div>
+          {/* Che tempo fa invece (soleggiato, piovoso ecc) qua */}
+          <div className="weather"></div>
+        </div>
       </main>
     </div>
   );

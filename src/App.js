2dbe5e92a1c82f68fe5ab86b45109d47c035802d
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-13 19:55:56 +0200

    Aggiunto immagini background e prime classi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
 
+/* Salvo la API della mia app meteo in una costante */
+const api = {
+  /* Key Generata */
+  key: "afaf9f8d48cff6cafd32e23220bcfdbf",
+  /* Da che sito la sto prendendo */
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+/* Verifico la mia API */
+console.log(api);
+
 function App() {
   return (
-    <div className="App">
+    <div className="app warm">
+      <main>
+        {/* Searchbox necessaria */}
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Cerca.."
+          />
+        </div>
 
+      </main>
     </div>
   );
 }
